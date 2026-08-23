32e57fc9d0af53a53a150f49c52962fab6cf2ed8
JOEboy1030 <aisakajoe1030@gmail.com>
2026-08-23 17:27:53 +0900

    手札配布

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -1,10 +1,15 @@
 package com.game;
 
+import java.util.Scanner;
+
 public class Main {
-    private static final int DEFAULTHAND = 10;
+    private static final int DEFAULTHAND = 30;
 
     public static void main(String[] args) {
         int hand = DEFAULTHAND;
+        // ゲームモードを数値で管理（１：一人用、２：対戦用）
+        int mode;
+        Scanner sc = new Scanner(System.in);
 
         if (args.length > 0) {
             try {
@@ -18,9 +23,12 @@ public class Main {
             }
 
         }
+
         System.out.println("ゲームを" + hand + "で開催します\n");
+        System.out.print("ゲームのモードを入力してください\n\n>");
+        mode = sc.nextInt();
         GameController gameController = new GameController(hand);
-        gameController.gameStart();
+        gameController.gameStart(mode);
 
     }
 }
